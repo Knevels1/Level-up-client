e658d8d7c4754ee19eda87a6da9abb3757dd0d7b
Knevels1 <kyle.a.nevels@icloud.com>
2021-02-18 04:15:31 -0600

    completed chapter 10

--- src/components/game/GameList.js
+++ src/components/game/GameList.js
@@ -24,7 +24,7 @@ export const GameList = (props) => {
                     games.map(game => {
                         return <section key={`game--${game.id}`} className="game">
                             <div className="game__title">Title: {game.title}</div>
-                            <div className="game__type">Game Type: {game.gametype.label}</div>
+                            <div className="game__type">Game Type: {game.game_type.label}</div>
                             <div className="game__players">Players required: {game.number_of_players}</div>
                             <div className="game__description">Description:  {game.description}</div>
                             <br />
